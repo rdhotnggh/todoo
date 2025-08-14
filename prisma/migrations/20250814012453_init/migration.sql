85ab38a1d5ab8d97f43edbea91908c975c43cae8
rdgotnggh <rdhotnggh@gmail.com>
2025-08-14 09:31:29 +0800

    first commit

---
+++ prisma/migrations/20250814012453_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `todo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `blabla` VARCHAR(10000) NOT NULL,
+    `dibuat` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `diperbarui` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
